5aab00dfb9a6e608c473d587ea969bf85ae0d1d9
disaster <disasterdev404@gmail.com>
2024-02-13 00:16:31 +0100

    main: added call to FileReader

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SaballutsWeatherLoader.Services;
+using Microsoft.Extensions.Configuration;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+.AddJsonFile("appsettings.json")
+.Build();
+
+FileReader sourceReader = new(configuration["CsvFilePath"]!);
+var csvWeatherRecords = sourceReader.Read();
+foreach (var record in csvWeatherRecords)
+{
+    System.Console.WriteLine($"Date: {record.Date} - Time: {record.Time}");
+}
